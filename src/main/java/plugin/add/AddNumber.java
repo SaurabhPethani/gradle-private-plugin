ade61fad419ced7efec2196ae89c1b3a100265ca
SaurabhPethani <spethani28@gmail.com>
2021-06-21 12:02:09 +0530

    Gradle plugin init

---
+++ src/main/java/plugin/add/AddNumber.java
@@ -0,0 +1,7 @@
+package plugin.add;
+
+public class AddNumber {
+    static public void printSum(int a, int b){
+        System.out.println("Addition is "+(a+b));
+    }
+}
---
+++ src/test/java/AddNumberTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import plugin.add.AddNumber;
+
+public class AddNumberTest {
+    @Test
+    void testAdd() {
+        AddNumber.printSum(1,2);
+    }
+}
